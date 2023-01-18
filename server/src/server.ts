b8dd8764127afce263b1419e8964dadae49ac815
juanevangelista1 <juan.evangelista.nascimentoo@gmail.com>
2023-01-17 22:38:28 -0300

    Update DataBase

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,29 +1,14 @@
-import fastify from "fastify";
+import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from "@prisma/client"
+import { appRoutes } from "./routes"
 
-
-
-const app = fastify();
-const prisma = new PrismaClient()
+const app = Fastify()
 
 app.register(cors)
-
-app.get('/hello', async () => {
-    const habits = await prisma.habit.findMany({
-        where: {
-            title: {
-                startsWith: 'Beber'
-            }
-        }
-    })
-
-    return habits
-})
+app.register(appRoutes)
 
 app.listen({
-    port: 3333
+  port: 3333,
 }).then(() => {
-    console.log('HTTP server is running')
+  console.log('HTTP Server running!')
 })
-
